aecc9df11e5feea7673af4885ded5752d660b4cc
marcus drum <marcusdrum123@gmail.com>
2023-03-21 18:39:32 +0100

    Initial Commit

---
+++ Charts1/Charts1App.swift
@@ -0,0 +1,17 @@
+//
+//  Charts1App.swift
+//  Charts1
+//
+//  Created by Mark on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Charts1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
